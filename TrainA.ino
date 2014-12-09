44038ef093fd69e9500f94ec115477e82f2b8279
Eslamunto <eslam.shala@gmail.com>
2014-12-09 19:52:55 +0200

    Add code for trains

---
+++ TrainA.ino
@@ -0,0 +1,152 @@
+#include <SoftwareSerial.h>
+
+SoftwareSerial mySerial(8, 9); // RX, TX
+
+//Define Pins
+int enableA = 12; int pinA1 = 11; int pinA2 = 10;
+boolean run; 
+int startTime;
+
+void setup()
+{
+
+	pinMode(enableA, OUTPUT);
+	pinMode(pinA1, OUTPUT);
+	pinMode(pinA2, OUTPUT);
+	run = true;
+	Serial.begin(9600);
+	Serial.println("Hello I am the nano");
+	mySerial.begin(9600);
+	startTime = millis();
+}
+
+void loop() // run over and over
+{
+	if (mySerial.available()) {
+
+
+		int receivedByNano = mySerial.read();
+		//Serial.write(receivedByNano);
+		// Serial.println("recv method");
+		//Serial.println("Greetings from nano, i received the following");
+		Serial.println(receivedByNano);
+
+
+
+		//Case 1 (49)
+
+		if(receivedByNano == 49) {
+			enableMotors();
+			forward(1);
+
+		}
+		else if(receivedByNano == 50) {
+			disableMotors();
+		}
+
+		//       if(run) {
+		//   
+		//   enableMotors();
+		//   forward(10000);
+		//       }
+
+
+		if (Serial.available()) {
+
+			mySerial.write(Serial.read());
+
+
+		}
+
+	}
+}
+
+void motorAOn() {
+	digitalWrite(enableA, HIGH);
+}
+//void motorBOn() {
+// digitalWrite(enableB, HIGH);
+//}
+//disable motors
+void motorAOff()
+{
+	digitalWrite(enableA, LOW);
+}
+//void motorBOff() {
+// digitalWrite(enableA, LOW);
+//}
+//motor A controls
+void motorAForward() {
+	digitalWrite(pinA1, HIGH);
+	digitalWrite(pinA2, LOW);
+}
+void motorABackward() {
+	digitalWrite(pinA1, LOW);
+	digitalWrite(pinA2, HIGH);
+}
+//motor B contorls
+//void motorBForward() {
+// digitalWrite(pinB1, HIGH);
+// digitalWrite(pinB2, LOW);
+//}
+//void motorBBackward() {
+// digitalWrite(pinB1, LOW);
+// digitalWrite(pinB2, HIGH);
+//}
+//coasting and braking
+void motorACoast() {
+	digitalWrite(pinA1, LOW);
+	digitalWrite(pinA2, LOW);
+}
+void motorABrake() {
+	digitalWrite(pinA1, HIGH);
+	digitalWrite(pinA2, HIGH);
+}
+//void motorBCoast() {
+// digitalWrite(pinB1, LOW);
+// digitalWrite(pinB2, LOW);
+//}
+//void motorBBrake() {
+// digitalWrite(pinB1, HIGH);
+// digitalWrite(pinB2, HIGH);
+//}
+//Define High Level Commands
+void enableMotors() {
+	motorAOn();
+	// motorBOn();
+}
+void disableMotors()
+{
+	motorAOff();
+	// motorBOff();
+}
+void forward(int time) {
+	motorAForward(); 
+	//motorBForward();
+	delay(time);
+}
+void backward(int time) {
+	motorABackward(); 
+	//motorBBackward();
+	delay(time);
+}
+void turnLeft(int time) {
+	motorABackward();
+	// motorBForward();
+	delay(time);
+}
+void turnRight(int time) {
+	motorAForward(); 
+	//motorBBackward();
+	delay(time);
+}
+void coast(int time) {
+	motorACoast();
+	//motorBCoast();
+	delay(time);
+}
+void brake(int time) {
+	motorABrake(); 
+	//motorBBrake(); 
+	delay(time);
+}
